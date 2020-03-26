2185bc9dd722ca780139a0eb74a456dd98977d77
wil-qua <william.quan@o2mail.de>
2020-03-26 16:49:22 +0100

    Merge branch 'events' of C:\Users\willi\Desktop\pythonProjects\FileOrganizer with conflicts.

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QTextEdit
+from PyQt5.QtCore import QPropertyAnimation, QRect
+import sys
+from stylesheets import *
 from app import *
 
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.title = "PyQt5 Window"
+        self.top = 100
+        self.left = 100
+        self.width = 680
+        self.height = 500
+
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+
+        self.button = QPushButton("Start", self)
+        self.button.move(30, 30)
+        self.button.clicked.connect(self.doAnimation)
+
+
+
 window()
-
-
-
 
